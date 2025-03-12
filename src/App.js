e5c9b5c31e1e6b4387078938dd9002eed4e5e147
Matheus Reis <135277320+MatheusAyra@users.noreply.github.com>
2025-03-12 16:57:50 -0300

    OpenDev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <p>OpenDev</p>
       </header>
+      <main>
+        <section className='box-titulo'>
+          <h1>OpenDev</h1>
+          <p>Trabalhando ReactJS, é uma biblioteca Java para criar SPAs</p>
+          < a href="https://reactjs.org/" target="_blank">Site Oficial</a>
+        </section>
+        <section className='box-requisitos'>
+          <h2>Requisitos</h2>
+          <p>Veja abaixo os requisitos mínimos para aprender React JS:</p>
+          <div className='box-cards'>
+            <div className='card'>
+              <h3>HTML</h3>
+              <p>Aprender a estrutura dos elementos da página com HTML</p>
+            </div>
+            <div className='card'>
+              <h3>CSS</h3>
+              <p>Aprender a estilizar os elementos da página com CSS</p>
+            </div>
+            <div className='card'>
+              <h3>Javascript</h3>
+              <p>Aprender a linguagem utilizada pelo REACT JS, o Javascript os elementosda página com CSS</p>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p className='autor'>Curso de Introdução ao REACT JS - OpenDev</p>
+      </footer>
     </div>
+
   );
 }
 
